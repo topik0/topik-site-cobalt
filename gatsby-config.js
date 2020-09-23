95325e131f3a6b011513b69992d840dccc4dfba9
Topik <topik@topik.tech>
2020-09-22 21:03:28 -0500

    Misc files

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
+  ],
+}
