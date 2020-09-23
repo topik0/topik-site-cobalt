35b618306fc736997a01bc38ec09f443b154d97e
Topik <topik@topik.tech>
2020-09-23 16:14:59 -0500

    Added button components

--- src/styles/primary.js
+++ src/styles/primary.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { Link } from "gatsby"
 
 export const MainContainer = styled.div`
   display: flex;
@@ -18,4 +19,37 @@ export const Container = styled.div`
   margin: ${props => (props.margin ? props.margin : "0")};
   position: ${props => (props.position ? props.position : "static")};
   z-index: ${props => (props.zindex ? props.zindex : "auto")};
+`
+export const BasicButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #222;
+  background-color: #fff;
+  width: 150px;
+  height: 50px;
+  color: #222;
+  border-radius: 12px;
+  margin: ${props => (props.margin ? props.margin : "none")};
+  text-decoration: none;
+  user-select: none;
+  cursor: pointer;
+  transition: all 0.2s;
+  &:hover {
+    transition: all 0.2s;
+    color: #fff;
+    background-color: #222;
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  }
+  &:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`
+export const BasicButtonExternal = styled(BasicButton).attrs({
+  as: "a",
+})``
+export const ButtonText = styled.p`
+  font-size: 20px;
 `
