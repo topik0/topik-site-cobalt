bb8987c812f8d35cea04d5fb7c3b41bc6cff2e00
Topik <topik@topik.tech>
2020-09-22 21:04:41 -0500

    Added text constants to be used by the entire site

---
+++ src/constants/text.js
@@ -0,0 +1,23 @@
+export const SplashBio = `I'm a front-end web developer from Chicago with a strong passion for technology and code. I have serveral years of experience with making websites for projects and clients.`
+export const AboutBio = `I am an experienced web developer from Chicago with a strong passion for technology and programming. I've been programming since I was ten and I have (hopefully) gotten much better since. I started programming by making basic websites and web applications for school and myself. Now, I am taking on many projects for clients and solutions that can help many people. One of my main goals is to solve problems that I've had trouble with. Check out my projects if you'd like to see what I'm working on or my past projects, and feel free to join my Discord or email me if you'd like to chat.
+`
+export const SkillsetBio = [
+  `Web Development with HTML/CSS, JS, React.js`,
+  `Robotics with Java using the FTC SDK`,
+  `Linux and UNIX based systems`,
+  `Computer hardware`,
+]
+export const RigText = `I’m currently maining a custom build that I made back in May of 2020.  Here are the specs:`
+export const RigBio = [
+  `Ryzen 7 3700x (8c, 16t, 4.2ghz OC)`,
+  `MSI B450-A Pro Max (ATX)`,
+  `Team T-Force Vulcan Z 16gb (2x8) DDR4-3200`,
+  `Crucial MX500 M.2 1TB`,
+  `Seagate Barracuda Compute 2TB 7200rpm`,
+  `Gigabyte RTX 2070 Super Windforce`,
+  `Phanteks P300a`,
+  `Phanteks AMP 600W`,
+  `MSI Optix G241 (1080p, 144hz, 23.8”)`,
+  `HP VH240a (1080, 60hz, 23.8”)`,
+  `Cooler Master Sk650 keyboard`,
+]
