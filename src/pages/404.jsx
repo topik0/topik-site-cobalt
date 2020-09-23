95791f0623666dd67ce1c2f0e4c931a995939c2a
Topik <topik@topik.tech>
2020-09-23 16:13:34 -0500

    Added a 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { LargeSplash } from "../styles/fourohfour.js"
+import { Paragraph } from "../styles/index.js"
+import {
+  BasicButton,
+  ButtonText,
+  Container
+} from "../styles/primary.js"
+
+export default function FourOFour() {
+  return (
+    <Container
+      horicenter="center"
+      vertcenter="center"
+      width="100%"
+      direction="column"
+    >
+      <LargeSplash size="300px">404</LargeSplash>
+      <Paragraph align="center">
+        The page you're looking for doesn't.. well.. exist
+      </Paragraph>
+      <BasicButton to="/" margin="30px">
+        <ButtonText>Go Home</ButtonText>
+      </BasicButton>
+    </Container>
+  )
+}
---
+++ src/styles/404.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const LargeSplash = styled.p`
+  font-weight: 900;
+  font-size: 300px;
+  margin-bottom: 20px;
+`
