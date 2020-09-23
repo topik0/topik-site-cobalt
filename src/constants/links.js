896759d98fc4d3fef11e4d71e86954ba3bbe043f
Topik <topik@topik.tech>
2020-09-22 21:04:22 -0500

    Added link constants for use by the navbar component

---
+++ src/constants/links.js
@@ -0,0 +1,8 @@
+export const links = [
+  { label: "Home", url: "/" },
+  { label: "About Me", url: "/about" },
+  { label: "Projects", url: "/projects" },
+  { label: "Open Source", url: "/opensource" },
+  { label: "Links", url: "/links" },
+]
+export default links
