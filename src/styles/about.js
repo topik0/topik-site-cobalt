4ea3aac099bd02bb7b7a0b7a21b2227970269898
Topik <topik@topik.tech>
2020-09-22 21:06:04 -0500

    Added styles to the about section

---
+++ src/styles/about.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+
+export const AboutContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  font-family: "Noto Serif KR", serif;
+  width: 100%;
+  height: 100vh;
+  color: #222;
+  flex-flow: wrap;
+`
+export const ParentCardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30%;
+`
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin: 60px;
+`
+export const CardTitle = styled.p`
+  font-size: 59px;
+  font-weight: 800;
+  margin-bottom: 25px;
+`
+export const CardBody = styled.p`
+  font-size: 23px;
+  line-height: 1.4;
+`
