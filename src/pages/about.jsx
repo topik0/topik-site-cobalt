1d33fc17b5f9a586f104a453f2aae980a1aaa609
Topik <topik@topik.tech>
2020-09-22 21:05:57 -0500

    Added an about section

---
+++ src/pages/about.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import {} from "../styles/primary.js"
+import { RigText } from "../constants/text"
+import Navbar from "../components/navbar/navbar.jsx"
+import {
+  AboutContainer,
+  ParentCardContainer,
+  CardContainer,
+  CardTitle,
+  CardBody,
+} from "../styles/about.js"
+import { AboutBio, SkillsetBio, RigBio } from "../constants/text"
+
+export default function About() {
+  return (
+    <AboutContainer>
+      <Navbar theme="dark" page="About Me" />
+      <ParentCardContainer>
+        <CardContainer>
+          <CardTitle>About Me</CardTitle>
+          <CardBody>{AboutBio}</CardBody>
+        </CardContainer>
+      </ParentCardContainer>
+      <ParentCardContainer>
+        <CardContainer>
+          <CardTitle>Skillset</CardTitle>
+          {SkillsetBio.map(skill => (
+            <CardBody>{skill}</CardBody>
+          ))}
+        </CardContainer>
+      </ParentCardContainer>
+      <ParentCardContainer>
+        <CardContainer>
+          <CardTitle>My Rig</CardTitle>
+          <CardBody>{RigText}</CardBody>
+          {RigBio.map(rig => (
+            <CardBody>{rig}</CardBody>
+          ))}
+        </CardContainer>
+      </ParentCardContainer>
+    </AboutContainer>
+  )
+}
