6a21a8ae3bfe6b6be49c487a26b5d5826e2f53ca
Topik <topik@topik.tech>
2020-09-26 17:08:51 -0500

    Removed opensource from the links file

--- src/constants/links.js
+++ src/constants/links.js
@@ -2,7 +2,6 @@ export const links = [
   { label: "Home", url: "/" },
   { label: "About Me", url: "/about" },
   { label: "Projects", url: "/projects" },
-  { label: "Open Source", url: "/opensource" },
   { label: "Links", url: "/links" },
 ]
 export const externals = [
