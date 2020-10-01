5b38eb658b4f3e2b70a63cc04395180608c4c1e9
Topik <topik@topik.tech>
2020-10-01 10:37:15 -0500

    Fixed the meta tag title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,12 +24,12 @@ export default function Home() {
         <meta name="theme-color" content="#111" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://topik.tech/" />
-        <meta property="og:title" content="Links - Topik Tech" />
+        <meta property="og:title" content="Topik Tech" />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={logoSrc} />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://topik.tech/" />
-        <meta property="twitter:title" content="Links - Topik Tech" />
+        <meta property="twitter:title" content="Topik Tech" />
         <meta property="twitter:description" content={description} />
         <meta property="twitter:image" content={logoSrc} />
       </Helmet>
