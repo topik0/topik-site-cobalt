12be05b8c2cc286e14e991108f345d0cc66051ff
Topik <topik@topik.tech>
2020-10-01 10:37:03 -0500

    Added %s

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   siteMetadata: {
     title: "Topik Tech",
-    titleTemplate: "Topik Tech",
+    titleTemplate: "%s - Topik Tech",
     description:
       "I'm a developer and tech enthusiast with a strong passion for technology. I have experience with frontend work with personal projects and client work.",
     url: "https://www.topik.tech",
