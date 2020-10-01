2eb9b1d656f9ef95ae2acda2c95e8c1d0026f383
Topik <topik@topik.tech>
2020-10-01 09:51:21 -0500

    Added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,21 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        icon: "src/images/favicon.png",
+        icon: "./src/images/favicon.png",
       },
     },
   ],
+  siteMetadata: {
+    title: "Topik Tech",
+    titleTemplate: "%s - Topik Tech",
+    description:
+      "I'm a developer and tech enthusiast with a strong passion for technology. I have experience with frontend work with personal projects and client work.",
+    url: "https://www.topik.tech",
+    image: "./src/images/favicon.png",
+  },
 }
